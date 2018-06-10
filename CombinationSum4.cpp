91aafa0b3fac72e8765152f82ec418383a3af8df
sourabh2k15 <sourab.reddy2k14@gmail.com>
2018-06-10 14:58:52 -0700

    Create CombinationSum4.cpp

---
+++ CombinationSum4.cpp
@@ -0,0 +1,23 @@
+// recursive version, to be optimized using DP
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        if(target == 0) return 1;        
+        
+        return combinationSum4(nums, 0, target);
+    }
+    
+    int combinationSum4(vector<int>& nums, int i, int target){
+        if(target == 0) return 1;
+        if(target < 0 || i == nums.size())  return 0;
+                
+        int ways = 0;
+        
+        for(int j = i; j < nums.size(); j++){
+            ways += combinationSum4(nums, i, target - nums[j]);
+        }
+        
+        return ways;
+    }
+};
